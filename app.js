66833a995c04963a0a654eebc32471372facffc6
howisrian <riansantos.dev@gmail.com>
2024-04-03 10:58:28 -0300

    Fix 1 Login e signup corrigidos

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const bcrypt = require('bcryptjs');
-
-const User = require('./models/user');
-
+const path = require('path');
+const usersFilePath = path.join(__dirname, 'data', 'users.json');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 // Middleware para analisar corpos de solicitação
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,36 +39,44 @@ app.post('/login', (req, res) => {
   }
 
   // Comparar senhas (certifique-se de usar bcrypt.compare se as senhas estiverem criptografadas)
-  if (user.password !== password) {
-    return res.status(401).send('Email ou senha incorretos');
-  }
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (err || !result) {
+      return res.status(401).send('Email ou senha incorretos');
+    }
 
-  // Redirecionar para a página de boas-vindas se a autenticação for bem-sucedida
-  res.redirect('/welcome');
+    // Redirecionar para a página de boas-vindas se a autenticação for bem-sucedida
+    res.redirect('/welcome');
+  });
 });
 
+app.use(express.urlencoded({ extended: true }));
 
-
-
+// Rota para lidar com a submissão do formulário de cadastro
 app.post('/signup', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Verificar se o usuário já existe
-    const existingUser = await User.findByEmail(email);
-    if (existingUser) {
-      return res.status(400).send('O email já está em uso.');
+    // Carregar dados de usuários existentes
+    const usersData = JSON.parse(fs.readFileSync(usersFilePath));
+
+    // Verificar se o email já está em uso
+    if (usersData.users.find(user => user.email === email)) {
+      return res.status(400).send('Erro: Email já está em uso');
     }
 
-    // Criar o novo usuário
-    const newUser = new User(email, password);
-    await newUser.save();
+    // Criptografar a senha
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Redirecionar o usuário para a página de boas-vindas
-    res.redirect('/welcome');
+    // Adicionar o novo usuário aos dados de usuários
+    usersData.users.push({ email, password: hashedPassword });
+
+    // Escrever os dados atualizados no arquivo JSON
+    fs.writeFileSync(usersFilePath, JSON.stringify(usersData, null, 2));
+
+    res.send('Usuário cadastrado com sucesso');
   } catch (error) {
-    console.error('Erro ao criar usuário:', error);
-    res.status(500).send('Ocorreu um erro ao criar o usuário.');
+    console.error('Erro ao adicionar usuário:', error);
+    res.status(500).send('Erro interno do servidor');
   }
 });
 
